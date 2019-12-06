d236d91dc9e608c116ecabbd68df8e25bb8f17c0
ezequielzacca <ezequielzacca@gmail.com>
2019-12-06 00:01:53 -0300

    step1

--- src/graphql/type-defs.ts
+++ src/graphql/type-defs.ts
@@ -9,54 +9,9 @@ export const typeDefs = gql`
     title: String!
   }
 
-  type Genre {
-    id: ID!
-    name: String!
-  }
-
-  type Author {
-    id: ID!
-    name: String!
-  }
-
-  input BookInput {
-    title: String!
-  }
-
-  input BookUpdateInput {
-    title: String
-  }
-
-  input GenreInput {
-    name: String!
-  }
-
-  input GenreUpdateInput {
-    name: String
-  }
-
-  input AuthorInput {
-    name: String!
-  }
-
-  input AuthorUpdateInput {
-    name: String
-  }
-
   # The "Query" type is the root of all GraphQL queries.
   # (A "Mutation" type will be covered later on.)
   type Query {
     books: [Book]
-    genres: [Genre]
-    authors: [Genre]
-  }
-
-  type Mutation {
-    createBook(book: BookInput): Book!
-    updateBook(id: ID!, book: BookUpdateInput!): Book!
-    createGenre(genre: GenreInput): Genre!
-    updateGenre(id: ID!, genre: GenreUpdateInput!): Genre!
-    createAuthor(author: AuthorInput): Author!
-    updateAuthor(id: ID!, author: AuthorUpdateInput!): Author!
   }
 `;
